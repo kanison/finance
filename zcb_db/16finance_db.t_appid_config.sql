5acd3e86740f03d76c0117e3df2865e3ff6e0966
kanison <kanison.zhang@foxmail.com>
2015-04-21 17:54:49 +0800

    change.

--- zcb_db/16finance_db.t_appid_config.sql
+++ zcb_db/16finance_db.t_appid_config.sql
@@ -3,7 +3,7 @@ use finance_db;
 
 DROP TABLE IF EXISTS t_appid_config;
 CREATE TABLE t_appid_config(	
-	FappId			int		COMMENT '应用id',
-	FstartNo 		int		COMMENT '开始自增序号',
+	FappId			varchar(10)		COMMENT '应用id',
+	FstartNo 		int				COMMENT '开始自增序号',
   PRIMARY KEY (`FappId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
